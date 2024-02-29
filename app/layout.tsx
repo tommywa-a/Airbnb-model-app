568e6600322c2213fa433836bdb938c66e701e58
tommywa-a <tomiwa95@gmail.com>
2024-02-29 15:47:27 +0100

    added/modified layout.tsx Container.tsx Logo.tsx Navbar.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,30 @@
-import type { Metadata } from "next";
-import { Inter,  Nunito } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter, Nunito } from 'next/font/google'
+import './globals.css'
+import Navbar from './components/navbar/Navbar'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Airbnb",
-  description: "Airbnb clone",
-};
+	title: 'Airbnb',
+	description: 'Airbnb clone',
+}
 
 const font = Nunito({
-  subsets: ["latin"],
-});
+	subsets: ['latin'],
+})
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
+		</html>
+	)
 }
