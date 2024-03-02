33386c3493c6b222acec38f4b1b9f6d6c593fb42
tommywa-a <tomiwa95@gmail.com>
2024-03-02 01:28:30 +0100

    created interface for Modal.tsx

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,6 +1,29 @@
-import React from 'react'
+interface ModalProps {
+  isOpen?: boolean
+  onClose: () => void
+  onSubmit: () => void
+  title?: string
+  body?: React.ReactElement
+  footer?: React.ReactElement
+  actionLabel: string
+  disabled?: boolean
+  secondaryAction?: () => void
+  secondaryActionLabel?: string
 
-const Modal = () => {
+}
+
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  onClose,
+  onSubmit,
+  title,
+  body,
+  footer,
+  actionLabel,
+  disabled,
+  secondaryAction,
+  secondaryActionLabel
+}) => {
   return (
     <div></div>
   )
