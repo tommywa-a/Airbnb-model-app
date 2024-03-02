85de945524ce9786b7e2125a205c2507be6d0c9e
tommywa-a <tomiwa95@gmail.com>
2024-03-02 01:19:11 +0100

    Started creation of Modal.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Nunito } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/navbar/Navbar'
 import ClientOnly from './components/ClientOnly'
+import Modal from './components/modals/Modal'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,6 +25,7 @@ export default function RootLayout({
 		<html lang='en'>
 			<body className={font.className}>
 				<ClientOnly>
+					<Modal />
         	<Navbar />
 				</ClientOnly>
 
