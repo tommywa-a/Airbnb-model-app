15909e20a6d97a0ea4261c5d16473d35e820058b
tommywa-a <tomiwa95@gmail.com>
2024-03-05 15:47:15 +0100

    Added google and githun sign-in buttons to RegisterModal.tsx

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -10,6 +10,7 @@ import Modal from './Modal'
 import Heading from '../Heading'
 import Input from '../inputs/Input'
 import toast from 'react-hot-toast'
+import Button from '../Button'
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal()
@@ -75,6 +76,34 @@ const {
     </div>
   )
 
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+      outline
+      label='Continue with Google'
+      icon={FcGoogle}
+      onClick={() => {}}
+      />
+      <Button
+      outline
+      label='Continue with Github'
+      icon={AiFillGithub}
+      onClick={() => {}}
+      />
+      <div className="text-neutral text-center mt-4 font-light">
+        <div className='justify-center flex flex-row items-center gap-2'>
+          <div>
+            Already have an account?
+          </div>
+          <div onClick={registerModal.onClose} className='text-neutral-800 cursor-pointer hover:underline'>
+            Log in
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
   return (
     <Modal
       disabled={isLoading}
@@ -84,6 +113,7 @@ const {
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     />
   )
 }
