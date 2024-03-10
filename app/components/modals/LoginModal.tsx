0b16b01ae8af1cf2e41b7ded297885e6ceb540d9
tommywa-a <tomiwa95@gmail.com>
2024-03-10 20:08:42 +0100

    social login with google and github, and added the avatar from social sign-in

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -86,13 +86,13 @@ const {
       outline
       label='Continue with Google'
       icon={FcGoogle}
-      onClick={() => {}}
+      onClick={() => signIn('google')}
       />
       <Button
       outline
       label='Continue with Github'
       icon={AiFillGithub}
-      onClick={() => {}}
+      onClick={() => signIn('github')}
       />
       <div className="text-neutral text-center mt-4 font-light">
         <div className='justify-center flex flex-row items-center gap-2'>
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -40,7 +40,7 @@ const UserMenu: React.FC<userMenuProps> = ({
 				>
 					<AiOutlineMenu />
 					<div className='hidden md:block'>
-						<Avatar />
+						<Avatar src={currentUser?.image} />
 					</div>
 				</div>
 			</div>
