a0d9bd2061c8ce0c08ddbf90dc8fb5f218d5c76d
tommywa-a <tomiwa95@gmail.com>
2024-03-11 08:08:24 +0100

    Added ability to be re-routed to home page by clicking on app logo

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -8,10 +8,11 @@ const Logo = () => {
 
 	return (
 		<Image
+			onClick={() => router.push('/')}
 			alt='Logo'
 			className='hidden md:block cursor-pointer'
-			height="100"
-			width="100"
+			height='100'
+			width='100'
 			src='/images/logo.png'
 		/>
 	)
