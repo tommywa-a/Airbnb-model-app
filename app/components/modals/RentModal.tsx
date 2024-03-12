13a4f9cf87fde5256c80aeb2ec572e6550c72a95
tommywa-a <tomiwa95@gmail.com>
2024-03-12 15:53:09 +0100

    useCountries hook

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,6 +9,7 @@ import Modal from './Modal'
 import Heading from '../Heading'
 import { categories } from '../navbar/Categories'
 import CategoryInput from '../inputs/CategoryInput'
+import CountrySelect from '../inputs/CountrySelect'
 
 enum STEPS {
   CATEGORY = 0,
@@ -102,11 +103,25 @@ const RentModal = () => {
     </div>
   )
 
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title="Where is your place located?"
+          subtitle="Help guests find you."
+        />
+          <CountrySelect
+          
+          />
+      </div>
+    )
+  }
+
   return (
     <Modal
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={rentModal.onClose}
+      onSubmit={onNext}
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
