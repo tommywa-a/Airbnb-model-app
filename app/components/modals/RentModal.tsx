77bd02b49e80d3a8e8da1361ad856e2e28d2366a
tommywa-a <tomiwa95@gmail.com>
2024-03-13 17:08:57 +0100

    Listing creation (step 3) - Image upload, Cloudinary CDN

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -12,6 +12,7 @@ import CategoryInput from '../inputs/CategoryInput'
 import CountrySelect from '../inputs/CountrySelect'
 import dynamic from 'next/dynamic'
 import Counter from '../inputs/Counter'
+import ImageUpload from '../inputs/ImageUpload'
 
 enum STEPS {
   CATEGORY = 0,
@@ -55,6 +56,7 @@ const RentModal = () => {
   const guestCount = watch('guestCount')
   const roomCount = watch('roomCount')
   const bathroomCount = watch('bathroomCount')
+  const imageSrc = watch('imageSrc')
   
 
   const Map = useMemo(() => dynamic(() => import('../Map'), {
@@ -160,6 +162,21 @@ const RentModal = () => {
           onChange={(value) => setCustomValue('bathroomCount', value)}
         />
         <hr />
+      </div>
+    )
+  }
+
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title='Add a photo of your place'
+          subtitle='Show guests what your place looks like!'
+        />
+        <ImageUpload
+          value={imageSrc}
+          onChange={(value) => setCustomValue('imageSrc', value)}
+        />
       </div>
     )
   }
