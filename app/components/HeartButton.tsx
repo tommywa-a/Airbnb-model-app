d07edbce07bf1db8edbfd1cbaff0e900ff182928
tommywa-a <tomiwa95@gmail.com>
2024-03-14 20:18:13 +0100

    Favoriting functionality, and minor bug fixes

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -2,19 +2,19 @@
 
 import {AiOutlineHeart, AiFillHeart} from 'react-icons/ai'
 import { User } from "@prisma/client"
+import useFavorite from '@/app/hooks/useFavorite'
 
 interface HeartButtonProps {
   listingId: string
-  currentUser: User | null
+  currentUser?: User | null
 }
 
 const HeartButton: React.FC<HeartButtonProps> = ({
   listingId,
   currentUser
 }) => {
-  const hasFavorited = false
-  const toggleFavorite = () => {}
-
+  const {hasFavorited, toggleFavorite} = useFavorite({listingId, currentUser})
+  
   return (
     <div
       onClick={toggleFavorite}
